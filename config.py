15ec96466af632a190f4d8b6f71b8157cc15c8ed
shridharshende <shridharshende@vctcpune.com>
2023-11-15 14:57:16 +0530

    db config

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ scaler_path=os.path.join("Model","scale.pkl")
 json_path=os.path.join("Model","project_data.json")
 
 DB_PASS = "admin"
-DB_PARAM = {'host': 'localhost', 'database': 'health', 'user': 'root', 'password': 'admin'}
+DB_PARAM = {'host': 'dshealth.culi2vvhaxwm.ap-south-1.rds.amazonaws.com', 'database': 'health', 'user': 'admin', 'password': 'rootroot'}
 
+DB_PARAM_LOCAL = {'host': 'localhost', 'database': 'health', 'user': 'root', 'password': 'admin'}
 # db password
 # root
 # vctc1234
